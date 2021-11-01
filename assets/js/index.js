5ab586fc501bb21e7fa6986500965ce653f14a3e
Radchenko-Valerii <85287763+Radchenko-Valerii@users.noreply.github.com>
2021-11-01 23:08:39 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d");
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeBottom = new Image();
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeUp = new Image();
+const pipeBottom = new Image();
 
 bird.src = "./assets/img/IMG_8002.png";
 bg.src = "./assets/img/flappy_bird_bg.png";
@@ -14,16 +14,9 @@ pipeUp.src = "./assets/img/flappy_bird_pipeUp.png";
 pipeBottom.src = "./assets/img/flappy_bird_pipeBottom.png";
 
 bird.height = 20;
-// Звуковые файлы
-// var fly = new Audio();
-// var score_audio = new Audio();
 
-// fly.src = "audio/fly.mp3";
-// score_audio.src = "audio/score.mp3";
+const gap = 90;
 
-var gap = 90;
-
-// При нажатии на какую-либо кнопку
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
@@ -31,24 +24,23 @@ function moveUp() {
  fly.play();
 }
 
-// Создание блоков
-var pipe = [];
+const pipe = [];
 
 pipe[0] = {
  x : cvs.width,
  y : 0
 }
 
-var score = 0;
-// Позиция птички
-var xPos = 10;
-var yPos = 150;
-var grav = 1.5;
+let score = 0;
+
+let xPos = 10;
+let yPos = 150;
+let grav = 1.5;
 
 function draw() {
  ctx.drawImage(bg, 0, 0);
 
- for(var i = 0; i < pipe.length; i++) {
+ for(let i = 0; i < pipe.length; i++) {
  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
@@ -61,12 +53,12 @@ function draw() {
  });
  }
 
- // Отслеживание прикосновений
+
  if(xPos + bird.width >= pipe[i].x
  && xPos <= pipe[i].x + pipeUp.width
  && (yPos <= pipe[i].y + pipeUp.height
  || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
- location.reload(); // Перезагрузка страницы
+ location.reload();
  }
 
  if(pipe[i].x == 5) {
